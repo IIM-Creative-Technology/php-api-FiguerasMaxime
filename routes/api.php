3e858b6ea79c67140ce51a400482ea12a6585d77
Figueras <maxime.figueras@gmail.com>
2021-03-12 18:08:51 +0100

    [Add] Routes CRUD Classe

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use GrahamCampbell\ResultType\Result;
+use App\Models\Classe;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,27 +21,29 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 //affichage de toutes les classes
 Route::get('/classes', function() {
-    return "Liste des classes";
+    return Classe::all();
 });
 
 //affichage de la classe sélectionnée
 Route::get('/classes/{ClasseId}', function($ClasseId) {
-    return "Voici la classe n°" . $ClasseId;
+    return Classe::findOrFail($ClasseId);
 });
 
 //modification des infos de la classe sélectionnée
 Route::put('/classes/{ClasseId}', function($ClasseId, Request $request) {
-
+    $classe = Classe::findOrFail($ClasseId);
+    $classe -> update($request->all());
+    return $classe;
 });
 
 //suppression d'une classe 
 Route::delete('/classes/{ClasseId}', function($ClasseId){
-
+    return Classe::findOrFail($ClasseId)->delete();
 });
 
 //ajout d'une classe
 Route::post('/classes', function(Request $request) {
-
+    return Classe::create($request->all());
 });
 
 
